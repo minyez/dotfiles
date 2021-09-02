88d0157454f582ca807bb6745f1ae1fad7434246
minyez <stevezhang@pku.edu.cn>
2021-09-02 22:27:37 +0800

    zsh filter agr for xmgrace, sdcv in Fedora install
    also dry run mode in the Fedora install script

--- install/amdfed.sh
+++ install/amdfed.sh
@@ -11,6 +11,9 @@ if (( IS_LINUX )); then
   FEDORA_VERSION=$(awk -F = '/VERSION_ID/ {print $2}' /etc/os-release)
 fi
 YES_NO_P=("No" "Yes")
+_DRY_RUN=1
+
+wgetopts="-nv --show-progress --progress=bar:force"
 
 cecho() {
   # colorize echo
@@ -18,6 +21,7 @@ cecho() {
     "s" | "success" ) color="\e[1;32m";;
     "e" | "error" ) color="\e[1;31m";;
     "i" | "info" ) color="\e[1;34m";;
+    "w" | "warn" ) color="\e[38;2;234;80;3m";;
     *) color="\e[0m"
   esac
   shift 1
@@ -31,6 +35,7 @@ help_info() {
   echo "  Your OS: $(uname -snr)"
   (( IS_LINUX )) && \
   echo "      CPU: $(lscpu | awk '/Model name/ {sep = ""; for (i = 3; i <= NF; i++) {printf("%s%s", sep, $i); sep=OFS}; printf("\n")}')"
+  (( _DRY_RUN )) && cecho w "Dry mode is switched on. Change _DRY_RUN to 0 to actually install"
 }
 
 check_prereq() {
@@ -56,16 +61,19 @@ done
 
 update_dnf() {
   cecho i "Updating Fedora..."
+  (( _DRY_RUN )) && return
   sudo dnf -y update
 }
 
 install_compiler() {
   cecho i "Installing compilers..."
+  (( _DRY_RUN )) && return
   sudo dnf --disablerepo="*" --enablerepo=fedora -y install gcc gfortran gcc-c++
 }
 
 install_config_tools() {
   cecho i "Installing config tools..."
+  (( _DRY_RUN )) && return
   sudo dnf -y install \
     make cmake autoconf automake git binutils binutils-devel tcsh \
     bzip2 gzip p7zip \
@@ -78,16 +86,23 @@ install_config_tools() {
 
 install_network_tools() {
   cecho i "Installing network tools..."
+  (( _DRY_RUN )) && return
   sudo dnf -y install openssl-devel curl libcurl-devel wget clash
 }
 
 install_texlive_full() {
   cecho i "Installing full texlive..."
+  (( _DRY_RUN )) && return
   sudo dnf -y install texlive-scheme-full
 }
 
 install_basic_python_tools() {
-  cecho i "Installing basic python tools..."
+  cecho i "Installing basic python tools with system pip..."
+  (( _DRY_RUN )) && return
+  if [[ $(which pip) != "/usr/bin/pip" ]]; then
+    cecho e "Error: not system pip, current pip = $(which pip)"
+    exit 2
+  fi
   sudo pip install rst2html
   cecho s "Installed rst2html"
 }
@@ -98,14 +113,28 @@ install_basic_python_tools() {
 #  sudo dnf -y install fcitx fcitx-{qt{4,5},table,configtool,table-{extra,other,chinese}}
 #  sudo dnf -y install gnome-tweaks
 #}
-install_rime() {
-  cecho i "Installing Rime..."
+install_language_tools() {
+  cecho i "Installing languages tools: Rime, sdcv..."
+  (( _DRY_RUN )) && return
   sudo dnf -y install ibus-rime
   ibus-daemon -drx
   cecho s "Rime installed, but you need to run ibus-setup to add Rime to input method"
+  sudo dnf -y install sdcv
+  cecho s "sdcv installed"
+  cecho i "Start dowloading zh_CN dictionary: "
+  sudo mkdir -p /usr/share/stardict/dic
+  wget $wgetopts http://download.huzheng.org/zh_CN/stardict-oxford-gb-2.4.2.tar.bz2
+  sudo tar -xjf stardict-oxford-gb-2.4.2.tar.bz2 -C /usr/share/stardict/dic
+  cecho s "sdcv dictionary installed"
 }
 
 help_info
 check_prereq
 
-install_rime
+update_dnf
+install_config_tools
+install_compiler
+install_network_tools
+install_texlive_full
+install_language_tools
+
