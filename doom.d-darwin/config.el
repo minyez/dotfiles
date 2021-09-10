6605935285049c09fddef09d5f976236eda77206
minyez <stevezhang@pku.edu.cn>
2021-09-10 11:53:02 +0800

    modify doom config on macos wrt fedora

--- doom.d-darwin/config.el
+++ doom.d-darwin/config.el
@@ -58,11 +58,16 @@
 ;(setq projectile-project-search-path '("/Users/stevezhang/Documents/SelfDevelopment/Codes"))
 
 ; Global variables
+;   org_notes : all my org(-roam) notes
+;   bibfile : bibliography, now a symlink to my exported zotero biblatex library
+;   mz/evil-leader : leader key to unify evil-leader and other keys that I want to emulate as evil-leader
+;                    the later usage is mainly when I only want to bind key to particular mode
 (setq
  org_notes (concat (getenv "HOME") "/Documents/SelfDevelopment/org-roam")
+ bibfile (concat org_notes "/bibliography_macos.bib")
  ;org_notes (concat (getenv "HOME") "/tmp/org-demo") ; for demonstration
- bibfile (concat (getenv "HOME") "/Documents/SelfDevelopment/org-roam/bibliography_macos.bib")
  ;bibfile (concat (getenv "HOME") "/Documents/SelfDevelopment/Database/test.bib")
+ mz/evil-leader ","
 )
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -110,6 +115,15 @@
   :config
   (setq vterm-shell "zsh"))
 
+(use-package! evil-leader
+  :config
+  ;; turn on the leader mode
+  (global-evil-leader-mode +1)
+  ;; note , as leader conflicts with evil-snipe evil-snipe-repeat-reverse
+  ;; bind it to alt+; in the evil-snipe session
+  (evil-leader/set-leader mz/evil-leader)
+)
+
 (use-package evil
   :bind
   (:map evil-insert-state-map
@@ -122,6 +136,13 @@
   (progn
   (evil-set-initial-state 'delve-mode 'emacs)
   )
+)
+
+(use-package! evil-snipe
+  ;; see L612 of evil-snipe
+  :bind
+  (:map evil-snipe-parent-transient-map
+        ("s-;" . evil-snipe-repeat-reverse))
 )
 
 (use-package! company
@@ -1780,4 +1801,26 @@ I appreciate anyone who reads this handout. Suggestions are totally welcome.
 
 ;; native-comp
 ;(setq comp-speed 1)
+
+(use-package! window-numbering
+  :config
+  (window-numbering-mode)
+  ;; redefine workspaces shortcuts to resolve the conflict with +doom/workspaces
+)
+
+(evil-leader/set-key
+; use doom tabs function and evil-leader to switch (centaur) tabs
+; learn lambda function to create closure for key binding
+; https://stackoverflow.com/a/1030409
+  "t1" (lambda () (interactive) (+tabs:next-or-goto 1))
+  "t2" (lambda () (interactive) (+tabs:next-or-goto 2))
+  "t3" (lambda () (interactive) (+tabs:next-or-goto 3))
+  "t4" (lambda () (interactive) (+tabs:next-or-goto 4))
+  "t5" (lambda () (interactive) (+tabs:next-or-goto 5))
+  "t6" (lambda () (interactive) (+tabs:next-or-goto 6))
+  "t7" (lambda () (interactive) (+tabs:next-or-goto 7))
+  "t8" (lambda () (interactive) (+tabs:next-or-goto 8))
+  "t9" (lambda () (interactive) (+tabs:next-or-goto 9))
+  "u"  'outline-up-heading
+)
 
