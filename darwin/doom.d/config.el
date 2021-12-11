cb69fbab9b42462e106b60d4e6c9d255d3d55bbe
minyez <stevezhang@pku.edu.cn>
2021-12-11 16:04:51 +0800

    move evil and pdf common

--- darwin/doom.d/config.el
+++ darwin/doom.d/config.el
@@ -131,48 +131,12 @@
 (add-to-list 'image-type-file-name-regexps '("\\.pdf\\'" . imagemagick))
 (add-to-list 'image-file-name-extensions "pdf")
 
+(require 'config-evil)
+
 ;; set zsh to default shell of vterm
 (use-package! vterm
   :config
   (setq vterm-shell "zsh"))
-
-(use-package! evil-leader
-  :config
-  ;; turn on the leader mode
-  (global-evil-leader-mode +1)
-  ;; note , as leader conflicts with evil-snipe evil-snipe-repeat-reverse
-  ;; bind it to alt+; in the evil-snipe session
-  (evil-leader/set-leader mz/evil-leader)
-)
-
-(use-package! evil
-  :bind
-  (:map evil-insert-state-map
-        ("s-i" . evil-escape))
-  (:map evil-normal-state-map
-        ("s-i" . evil-insert))
-  (:map evil-visual-state-map
-        ("s-i" . evil-insert))
-  :config
-  (progn
-  (evil-set-initial-state 'delve-mode 'emacs)
-  )
-)
-
-;(use-package! evil-collection
-;  :after evil
-;  :ensure t
-;  :config
-;  (evil-collection-init)
-;)
-
-(use-package! evil-snipe
-  ;; see L612 of evil-snipe
-  :after evil
-  :bind
-  (:map evil-snipe-parent-transient-map
-        ("s-;" . evil-snipe-repeat-reverse))
-)
 
 (use-package! ws-butler
   :config
@@ -336,157 +300,7 @@
 ;  (org-icalendar-after-save . org-icalendar-open-ics-file)
 ;)
 
-(use-package org-pdftools
-  :hook (org-load . org-pdftools-setup-link))
-
-;(use-package org-noter-pdftools
-;  :after org-noter
-;  :config
-;  (with-eval-after-load 'pdf-annot
-;    (add-hook 'pdf-annot-activate-handler-functions #'org-noter-pdftools-jump-to-note)))
-
-(use-package! pdf-view
-  :config
-  (map! :map pdf-view-mode-map
-        :nv "z g" #'pdf-view-goto-page
-        :nv "z r" #'image-rotate       ;; rotate the page (defined in image.el)
-        )
-)
-
-(use-package! org-noter
-  :after (:any org pdf-view)
-  :config
-  (setq
-   ;; The WM can handle splits
-   ;;org-noter-notes-window-location 'other-frame
-   ;; Please stop opening frames
-   org-noter-always-create-frame nil
-   ;; I want to see the whole file
-   org-noter-hide-other nil
-   ;; Everything is relative to the main notes file
-   org-noter-notes-search-path (list mz/org-notes)
-   ;org-noter-set-notes-window-behavior 'scroll
-   ;; split fraction. default (0.5 . 0.5). slightly larger on vertical
-   org-noter-doc-split-fraction '(0.56 . 0.5)
-  )
- (defun my-org-noter-extract-annotation ()
-   "Create notes skeleton with the PDF annotations.
- Only available with PDF Tools."
-   (interactive)
-   (org-noter--with-valid-session
-    (cond
-    ( (eq (org-noter--session-doc-mode session) 'pdf-view-mode)
-      (let* ((ast (org-noter--parse-root))
-             (top-level (org-element-property :level ast))
-             output-data)
-        (with-current-buffer (org-noter--session-doc-buffer session)
-            (let ((chosen-annots (list 'highlight
-                                       'underline
-                                       'squiggly
-                                       'text
-                                       'strike-out
-                                      ))
-                  insert-contents pages-with-links)
- 
-              (setq insert-contents t)
- 
-              (dolist (item (pdf-info-getannots))
-                (let* ((type  (alist-get 'type item))
-                       (page  (alist-get 'page item))
-                       (edges (or (org-noter--pdf-tools-edges-to-region (alist-get 'markup-edges item))
-                                  (alist-get 'edges item)))
-                       (top (nth 1 edges))
-                       (item-subject (alist-get 'subject item))
-                       (item-contents (alist-get 'contents item))
-                       name contents)
-                  (when (and (memq type chosen-annots) (> page 0))
-                    (if (eq type 'link)
-                        (cl-pushnew page pages-with-links)
-                      (setq name (cond ((eq type 'highlight)  "Highlight")
-                                       ((eq type 'underline)  "Underline")
-                                       ((eq type 'squiggly)   "Squiggly")
-                                       ((eq type 'text)       "Text note")
-                                       ((eq type 'strike-out) "Strikeout")))
-                      (when insert-contents
-                        (setq contents (cons (pdf-info-gettext page edges)
-                                             (and (or (and item-subject (> (length item-subject) 0))
-                                                      (and item-contents (> (length item-contents) 0)))
-                                                  (concat (or item-subject "")
-                                                          (if (and item-subject item-contents) "\n" "")
-                                                          (or item-contents ""))))))
- 
-                      (push (vector (format "%s on page %d" name page) (cons page top) 'inside contents)
-                            output-data)
-            )
-            )))
-        )
- 
-          (when output-data
-            (setq output-data
-                  (sort output-data
-                        (lambda (e1 e2)
-                          (or (not (aref e1 1))
-                              (and (aref e2 1)
-                                   (org-noter--compare-locations '< (aref e1 1) (aref e2 1)))))))
-      )
-      )
-      ; print out
-        (with-current-buffer (org-noter--session-notes-buffer session)
-          ;; NOTE(nox): org-with-wide-buffer can't be used because we want to reset the
-          ;; narrow region to include the new headings
-          (widen)
-          (save-excursion
-            (goto-char (org-element-property :end ast))
- 
-            (let (last-absolute-level
-                  title location relative-level contents
-                  level)
-              (dolist (data output-data)
-                (setq title          (aref data 0)
-                      location       (aref data 1)
-                      ;relative-level (aref data 2)
-                      contents       (aref data 3))
-                ;(if (symbolp relative-level)
-                ;    (setq level (1+ last-absolute-level))
-                ;  (setq last-absolute-level (+ top-level relative-level)
-                ;        level last-absolute-level))
-                (setq level (1+ top-level))
- 
-                ;;; add by minyez
-                ;(org-noter--insert-heading level title)
-                (when (car contents)
-                  (org-noter--insert-heading level (remove-line-breaks (car contents))))
-                ;; end add by minyez
- 
-                (when location
-                  (org-entry-put nil org-noter-property-note-location (org-noter--pretty-print-location location)))
- 
-                (when org-noter-doc-property-in-notes
-                  (org-entry-put nil org-noter-property-doc-file (org-noter--session-property-text session))
-                  (org-entry-put nil org-noter--property-auto-save-last-location "nil"))
- 
-                (when title
-                  (org-noter--insert-heading (1+ level) title))
-                (when (cdr contents)
-                  (org-noter--insert-heading (1+ level) "Comment")
-                  ;(insert (cdr contents)))
-                  (insert (remove-line-breaks (cdr contents))))
-         ))
- 
-            (setq ast (org-noter--parse-root))
-            (org-noter--narrow-to-root ast)
-            (goto-char (org-element-property :begin ast))
-            (outline-hide-subtree)
-            (org-show-children 1)
-      )
-      )
-    )
-    )
-    (t (user-error "This command is only supported on PDF Tools.")))))
-   :bind
-   ("C-c n N" . org-noter)
-   ("C-c n s" . my-org-noter-extract-annotation)
-)
+(require 'config-pdf)
 
 ;http://ergoemacs.org/emacs/modernization_elisp_lib_problem.html
 ;(defun s-trim-newline (string)
@@ -764,27 +578,6 @@
   ;; redefine workspaces shortcuts to resolve the conflict with +doom/workspaces
 )
 
-(evil-leader/set-key
-; use doom tabs function and evil-leader to switch (centaur) tabs
-; learn lambda function to create closure for key binding
-; https://stackoverflow.com/a/1030409
-  "t1" (lambda () (interactive) (+tabs:next-or-goto 1))
-  "t2" (lambda () (interactive) (+tabs:next-or-goto 2))
-  "t3" (lambda () (interactive) (+tabs:next-or-goto 3))
-  "t4" (lambda () (interactive) (+tabs:next-or-goto 4))
-  "t5" (lambda () (interactive) (+tabs:next-or-goto 5))
-  "t6" (lambda () (interactive) (+tabs:next-or-goto 6))
-  "t7" (lambda () (interactive) (+tabs:next-or-goto 7))
-  "t8" (lambda () (interactive) (+tabs:next-or-goto 8))
-  "t9" (lambda () (interactive) (+tabs:next-or-goto 9))
-  "u"  'outline-up-heading
-)
-
-(map!
-  :nv (concat mz/evil-leader " w |") #'split-window-below
-  :nv (concat mz/evil-leader " w -") #'split-window-right
-)
-
 ;;; for use of direnv
 ; envrc from Doom :tool direnv by purcell
 (use-package! envrc
@@ -827,13 +620,6 @@
              ))
          )
   )
-)
-
-; activate
-(use-package! pdf-tools
-  :config
-  ;; activate when opening pdf, otherwise the evil keybindings will not work
-  (pdf-loader-install)
 )
 
 ;(setq ns-auto-hide-menu-bar t)
