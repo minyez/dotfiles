8dca792ffa262744a822fc8069cf07e7e8744c73
minyez <stevezhang@pku.edu.cn>
2022-04-25 23:36:48 +0800

    move neovim-lua to common

--- common/neovim-lua/lua/plugins.lua
+++ common/neovim-lua/lua/plugins.lua
@@ -1,4 +1,35 @@
 -- original file from https://github.com/ravenxrz/dotfiles/blob/master/nvim/lua/user/plugins.lua
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    -- open_fn = function()
+    --   return require("packer.util").float { border = "rounded" }
+    -- end,
+  },
+}
 
 return require("packer").startup(function()
 -- =======================
@@ -50,4 +81,9 @@ return require("packer").startup(function()
     }
   }
 --  use "hrsh7th/cmp-nvim-lsp"
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
