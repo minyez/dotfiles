003ec8be793015cb3eaa4442d80d9840f68d1b4c
Minye Zhang <minyez.physchem@gmail.com>
2023-04-05 22:22:10 +0200

    turn to common nvim config

--- taiyi/neovim-lua/init.lua
+++ taiyi/neovim-lua/init.lua
@@ -1,7 +1,1 @@
--- User directory
-USER_DIR = "mynvim"
-
-require(USER_DIR .. '.plugins')
-require(USER_DIR .. '.keymaps')
-require(USER_DIR .. '.options')
-require(USER_DIR .. '.ui')
+../../common/neovim-lua/init.lua
