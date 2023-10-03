73feae40786c62c9e3a1ab50c07c72138f943258
Minye Zhang <minyez.physchem@gmail.com>
2023-10-03 19:18:19 +0200

    C-e to abort completion, instead of select first

--- common/neovim-lua/lua/editor/cmp.lua
+++ common/neovim-lua/lua/editor/cmp.lua
@@ -68,11 +68,11 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
     ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    -- ["<C-e>"] = cmp.mapping {
-    --   i = cmp.mapping.abort(),
-    --   c = cmp.mapping.close(),
-    -- },
-    ["<C-e>"] = cmp.mapping.confirm { select = true },
+    ["<C-e>"] = cmp.mapping {
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    },
+    -- ["<C-e>"] = cmp.mapping.confirm { select = true },
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm { select = false },
